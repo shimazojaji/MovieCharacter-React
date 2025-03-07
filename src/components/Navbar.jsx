c4aea411fb8ed48ba0bd7f272016b149b41601f3
shimazojaji <shima.zojaji@gmail.com>
2025-03-07 22:09:40 +0330

    fifth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,7 @@ function Logo() {
     <span className="navbar__logo">
       <svg
         xmlns="http://www.w3.org/2000/svg"
+         xmlnsXlink="http://www.w3.org/1999/xlink"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth="1.5"
